a88fa18765744a0a76162148a79f26564ae9e260
Md-Hiccup <ask2md@gmail.com>
2018-07-23 18:42:08 +0530

    fixing login/register issues

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,7 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-const passport = require('passport');
+const passport = require("passport");
 
 //  Load User Model
 const User = require("../../models/User");
@@ -97,9 +97,16 @@ router.post("/login", (req, res) => {
 //  @route  GET api/users/current
 //  @desc   Return current user
 //  @access Private
-router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.json({ msg: 'success'})
-})
-
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
