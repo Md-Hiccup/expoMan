e7fe781aea4995965003d5df054879f264050bc9
Md-Hiccup <ask2md@gmail.com>
2018-07-23 16:14:21 +0530

    login / signup done

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://jack:jack123@ds145921.mlab.com:45921/expmanager"
+  mongoURI: "mongodb://jack:jack123@ds145921.mlab.com:45921/expmanager",
+  secretOrKey:  'secret'
 };
--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const items = require("./routes/api/items");
 
 const app = express();
 
-// DB Config
+//  Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
+//  DB Config
 const db = require("./config/keys").mongoURI;
 
-// Connect to MongoDB
+//  Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello"));
+//  Passport Middleware
+app.use(passport.initialize());
+
+//  Passport Config
+require('./config/passport')(passport);
+
+//  Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/items", items);
 
 const port = process.env.PORT || 5000;
 
