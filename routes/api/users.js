ccdb77f186111209d07af27ef84c705f03ee2f5f
Md-Hiccup <ask2md@gmail.com>
2018-07-24 14:35:07 +0530

    done server side validation for login/register

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//  Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 //  Load User Model
 const User = require("../../models/User");
 
@@ -18,9 +22,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 //  @desc   Register / Signup user
 //  @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //  Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //  Size
@@ -37,6 +49,7 @@ router.post("/register", (req, res) => {
 
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
           newUser.password = hash;
           newUser
             .save()
@@ -52,6 +65,12 @@ router.post("/register", (req, res) => {
 //  @desc   Login User / Returning JWT Token
 //  @access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //  Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   const email = req.body.email;
   const password = req.body.password;
 
@@ -59,7 +78,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     //  Check for user
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = "User not found";
+      return res.status(404).json(errors);
     }
 
     //  Check Password
@@ -88,7 +108,8 @@ router.post("/login", (req, res) => {
         );
         // res.json({ msg: "Success" });
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "Password incorrect";
+        return res.status(400).json(errors);
       }
     });
   });
